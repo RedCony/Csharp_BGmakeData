69181d7c50e997dea7cab989dc0767d12989c894
RedCony <68505636+RedCony@users.noreply.github.com>
2022-01-14 00:47:15 +0900

    최초생성파일

---
+++ BGmakeData/App.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace BGmakeData
+{
+    class App
+    {
+        public App() 
+        {
+            //Console.WriteLine("BGmakeData");
+
+            string data = File.ReadAllText("./Bgmakelist.json");
+            //Console.WriteLine(data);
+
+            BGmakeData[] bGmakeDatas = JsonConvert.DeserializeObject<BGmakeData[]>(data);
+
+            Dictionary<int, BGmakeData> dicbgmakedata = new Dictionary<int, BGmakeData>();
+
+            foreach(BGmakeData bGmake in bGmakeDatas)
+            {
+                dicbgmakedata.Add(bGmake.id,bGmake);
+            }
+
+            foreach(KeyValuePair<int,BGmakeData> kv in dicbgmakedata)
+            {
+                Console.WriteLine
+                    ("\nID:{0},\n이름:{1},\n만들기정보:{2},\n실내버전:{3},\n파괴버전:{4},\n중요:{5},\n설명:{6}",
+                    kv.Key,kv.Value.name,kv.Value.mkinfo,kv.Value.insidever,kv.Value.destroyver,kv.Value.important,kv.Value.explain
+                    );
+            }
+        }
+    }
+}
